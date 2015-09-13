d73993ea6d5629cc15ec0eba41661c2b9bc5eae4
qinqiang1990 <1247686880@qq.com>
2015-09-13 10:46:36 +0800

    java module

---
+++ common/src/main/java/qinq/IAlgorithm.java
@@ -0,0 +1,5 @@
+package qinq;
+
+public interface IAlgorithm {
+    public float plus(float x, float y);
+}
---
+++ common/src/main/java/qinq/impl/Algorithm.java
@@ -0,0 +1,10 @@
+package qinq.impl;
+
+import qinq.IAlgorithm;
+
+public class Algorithm implements IAlgorithm {
+    @Override
+    public float plus(float x, float y) {
+        return x+y;
+    }
+}
