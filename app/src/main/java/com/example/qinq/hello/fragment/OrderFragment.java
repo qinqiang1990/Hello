8784b4bfee29415a8246c2622f86a72a3c9d0874
qinqiang1990 <1247686880@qq.com>
2015-09-13 10:04:26 +0800

    java module

--- app/src/main/java/com/example/qinq/hello/fragment/OrderFragment.java
+++ app/src/main/java/com/example/qinq/hello/fragment/OrderFragment.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.qinq.hello.R;
 import com.example.qinq.hello.ioc.view.ContentView;
+
+import qinq.IAlgorithm;
+import qinq.impl.Algorithm;
 
 /**
  * Created by qinqiang on 2015/8/27.
@@ -15,10 +20,15 @@ import com.example.qinq.hello.ioc.view.ContentView;
 @ContentView(R.layout.fragment_order)
 public class OrderFragment extends BaseFragment {
 
+    IAlgorithm algorithm;
+    Button sum;
+    TextView tv;
 
     @Override
     protected void initialize(View v) {
-
+        algorithm = new Algorithm();
+        sum = (Button) v.findViewById(R.id.order_sum);
+        tv = (TextView) v.findViewById(R.id.order_desc);
     }
 
     @Override
@@ -28,6 +38,13 @@ public class OrderFragment extends BaseFragment {
 
     @Override
     protected void addListener() {
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                tv.setText(String.valueOf(algorithm.plus(15f, 20f)));
+
+            }
+        });
     }
 }
